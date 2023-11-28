b95b3d80d64cac94929cfbed0fd4559eef5334cf
Kepsondiaz <kherabadiaby328@gmail.com>
2023-11-28 19:40:05 +0000

    add svg netflix logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
   return (
     <div className="flex items-center justify-around p-4 z-[100] w-full absolute">
-        <h1 className="text-red-600 text-4xl p-4 font-bold cursor-pointer">Netflix</h1>
+      <Link to="/">
+          <img className="w-24" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix logo" />
+      </Link>
         <div>
             <button className="bg-red-600 text-white  px-6 py-2 rounded cursor-pointer">Sign In</button>
         </div>
