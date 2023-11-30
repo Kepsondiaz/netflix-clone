76f49cc7322bbbda7986fb963f7211ab98ac5b69
Kepsondiaz <kherabadiaby328@gmail.com>
2023-11-30 12:12:35 +0000

    add logic in Navabar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,43 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { UserAuth } from "../Authentification/AuthContext"
 
 const Navbar = () => {
+
+  const {user, logOut} = UserAuth()
+
+  const navigation  = useNavigate()
+
+  const handleSubmit = async() => {
+    try {
+      await logOut()
+      navigation('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="flex items-center justify-around p-4 z-[100] w-full absolute">
       <Link to="/">
-          <img className="w-24" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix logo" />
+          <img className="w-32" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix logo" />
       </Link>
+      {user?.email ? (
         <div>
-            <button className="bg-red-600 text-white  px-6 py-2 rounded cursor-pointer">Sign In</button>
+            <Link to={'/account'}>
+                <button className="text-white pr-4">Account</button>
+            </Link>
+              <button 
+              onClick={() => handleSubmit()}
+              className="bg-red-600 text-white  px-6 py-2 rounded cursor-pointer">Logout</button>
         </div>
+      ) : (
+          <div>
+            <Link to={'/login'}>
+                <button className="bg-red-600 text-white  px-6 py-2 rounded cursor-pointer">Sign In</button>
+            </Link>
+          </div>
+      )
+    }
     </div>
   )
 }
