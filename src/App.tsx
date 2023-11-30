917bd423c0eca977d157b4aec10e5683956c75c6
Kepsondiaz <kherabadiaby328@gmail.com>
2023-11-30 12:13:38 +0000

    define signup, login, signout, account routes'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import { AuthContextProvider } from './Authentification/AuthContext'
+import Singup from './pages/Singup'
+import Login from './pages/Login'
+import Account from './pages/Account'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
     <>
-     <Navbar/>
-      <Routes>
-          <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvider >
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>} />
+                <Route path='/signup' element={<Singup />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/account' element={<ProtectedRoute><Account /></ProtectedRoute> } />
+            </Routes> 
+      </AuthContextProvider>
+
     </>
   )
 }
